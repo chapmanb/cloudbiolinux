0f6707f11a5ed477cda00bc2f17c647d95d495ab
chapmanb <chapmanb@50mail.com>
2011-05-31 10:58:45 -0400

    Move standard edition classes to single file

--- cloudbio/edition/base.py
+++ cloudbio/edition/base.py
@@ -1,4 +1,6 @@
-"""Base edition supplying CloudBioLinux functionality which can be customized.
+"""Base editions supplying CloudBioLinux functionality which can be customized.
+
+These are a set of testing and supported edition classes.
 """
 from fabric.api import *
 
@@ -35,11 +37,56 @@ class Edition:
         return list
 
     def apt_upgrade_system(self):
-        """Upgrade system through apt - so this behaviour can be 
-        overridden
+        """Upgrade system through apt - so this behaviour can be overridden
         """
         sudo("apt-get -y --force-yes upgrade")
 
     def post_install(self):
         """Post installation hook"""
 
+class BioNode(Edition):
+    """BioNode specialization of BioLinux
+    """
+    def __init__(self, env):
+        Edition.__init__(self,env)
+        self.name = "BioNode Edition"
+        self.short_name = "bionode"
+
+    def check_packages_source(self):
+        # Bionode removes sources, just to be sure
+        self.env.logger.debug("Clearing %s" % self.env.sources_file)
+        sudo("cat /dev/null > %s" % self.env.sources_file)
+
+class Minimal(Edition):
+    """Minimal specialization of BioLinux
+    """
+    def __init__(self, env):
+        Edition.__init__(self, env)
+        self.name = "Minimal Edition"
+        self.short_name = "minimal"
+
+    def check_packages_source(self):
+        pass
+
+    def rewrite_apt_sources_list(self, sources):
+        """Allows editions to modify the sources list. Minimal only
+           uses the default packages
+        """
+        return []
+
+    def rewrite_apt_automation(self, list):
+        """Allows editions to modify the apt automation list
+        """
+        return []
+
+    def rewrite_apt_keys(self, list):
+        """Allows editions to modify key list"""
+        return []
+
+    def rewrite_apt_keyserver(self, list):
+        """Allows editions to modify key list"""
+        return []
+
+    def apt_upgrade_system(self):
+        """Do nothing"""
+        env.logger.debug("Skipping forced system upgrade")
