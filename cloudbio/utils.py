1a2ee5b67333e60a7629c393d258b90ba6f24044
chapmanb <chapmanb@50mail.com>
2011-07-20 06:54:23 -0400

    Merge in Pjotr's logging and ruby configuration

--- cloudbio/utils.py
+++ cloudbio/utils.py
@@ -1,4 +1,8 @@
+"""Utilities for logging and progress tracking.
+"""
 import logging
+
+from fabric.api import sudo
 
 def _setup_logging(env):
     env.logger = logging.getLogger("cloudbiolinux")
@@ -12,3 +16,26 @@ def _setup_logging(env):
     ch.setFormatter(formatter)
     env.logger.addHandler(ch)
 
+def _update_biolinux_log(env, target, flavor):
+    """Updates the VM so it contains information on the latest BioLinux
+       update in /var/log/biolinux.log.
+
+       The latest information is appended to the file and can be used to see if
+       an installation/update has completed (see also ./test/test_vagrant).
+    """
+    if not target:
+        target = env.get("target", None)
+        if not target:
+            target = "unknown"
+        else:
+            target = target.name
+    if not flavor:
+        flavor = env.get("flavor", None)
+        if not flavor:
+            flavor = "unknown"
+        else:
+            flavor = flavor.name
+    logfn = "/var/log/biolinux.log"
+    info = "Target="+target+"; Edition="+env.edition.name+"; Flavor="+flavor
+    env.logger.info(info)
+    sudo("date +\"%D %T - Updated "+info+"\" >> "+logfn)
