df4c65f532f82bd88a8befe8da57ab4938f7203b
Pjotr Prins <pjotr.public01@thebird.nl>
2011-08-08 09:21:16 +0200

    Vagrant renamed 'vagrant' to 'default'

--- test/testlib/test_biolinux.rb
+++ test/testlib/test_biolinux.rb
@@ -4,7 +4,7 @@
 
 def test_for_completed_install(edition_name)
   # test for completion of the fabfile
-  run "scp vagrant:/var/log/biolinux.log biolinux.log"
+  run "scp default:/var/log/biolinux.log biolinux.log"
   lastline = `tail -1 biolinux.log`
   print lastline
   error "Installation looks incomplete "+lastline if lastline !~ /#{edition_name}/
