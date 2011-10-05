f65cad481dde92121ca49afd5e160999062744ec
Pjotr Prins <pjotr.public01@thebird.nl>
2011-10-05 08:29:45 +0200

    BEAST-mcmc package and tools added

--- cloudbio/custom/phylogeny.py
+++ cloudbio/custom/phylogeny.py
@@ -20,3 +20,16 @@ def install_tracer(env):
         env.safe_sudo("mv -f Tracer_v1.5 /usr/local/bioinf/tracer")
         env.safe_sudo("ln -sf /usr/local/bioinf/tracer/bin/tracer /usr/local/bin/tracer")
 
+@_if_not_installed("beast")
+def install_beast(env):
+    """BEAST for phylogeny
+    """
+    if not exists("/usr/local/bin/beast"):
+        run("wget http://beast-mcmc.googlecode.com/files/BEASTv1.6.2.tgz")
+        run("tar xvzf BEASTv1.6.2.tgz")
+        env.safe_sudo("mkdir -p /usr/local/bioinf")
+        env.safe_sudo("rm -rvf /usr/local/bioinf/beast")
+        env.safe_sudo("mv -f BEASTv1.6.2 /usr/local/bioinf/beast")
+        for l in ["beast","beauti","loganalyser","logcombiner","treeannotator","treestat"]:
+            env.safe_sudo("ln -sf /usr/local/bioinf/beast/bin/%s /usr/local/bin/%s" % (l,l))
+
