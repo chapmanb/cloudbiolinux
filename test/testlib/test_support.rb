19c1a861893ce165892e9561f6d1d9020aa5b8b8
Pjotr Prins <pjotr.public01@thebird.nl>
2011-12-20 12:22:19 +0100

    Testing: executing fab

--- test/testlib/test_support.rb
+++ test/testlib/test_support.rb
@@ -1,12 +1,16 @@
 # Run a command line command, without intercepting stdout and stderr
 def run cmd
   print "--> ",cmd
-  system(cmd)  # don't use backticks for fab!
+  res = system(cmd)  # don't use backticks for fab!
+  if res == nil
+    error "Command failed!"
+  end
+  res
 end
 
 # Error handler
 def error msg
-  $stderr.print "FATAL ERROR: ",msg
+  $stderr.print "\nFATAL ERROR: ",msg,"\n"
   exit 1
 end
 
