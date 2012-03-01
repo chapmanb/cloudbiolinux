9559bb823d20c68fe69789c4bcc131031f6f4266
Enis Afgan <afgane@gmail.com>
2012-03-01 14:17:08 +1100

    Add a custom log formatter that adjusts log messages based on log message level

--- cloudbio/utils.py
+++ cloudbio/utils.py
@@ -3,6 +3,39 @@
 import logging
 
 from fabric.api import sudo
+from fabric.colors import yellow, red, green, magenta
+
+class ColorFormatter(logging.Formatter):
+    """ Format log message based on the message level
+        http://stackoverflow.com/questions/1343227/can-pythons-logging-format-be-modified-depending-on-the-message-log-level
+    """
+    # Setup formatters for each of the levels
+    err_fmt  = red("ERR [%(filename)s(%(lineno)d)] %(msg)s")
+    warn_fmt  = magenta("WARN [%(filename)s(%(lineno)d)]: %(msg)s")
+    dbg_fmt  = yellow("DBG [%(filename)s]: %(msg)s")
+    info_fmt = green("INFO: %(msg)s")
+
+    def __init__(self, fmt="%(name)s %(levelname)s: %(msg)s"):
+        logging.Formatter.__init__(self, fmt)
+
+    def format(self, record):
+        # Save the original format configured by the user
+        # when the logger formatter was instantiated
+        format_orig = self._fmt
+        # Replace the original format with one customized by logging level
+        if record.levelno == 10:   # DEBUG
+            self._fmt = ColorFormatter.dbg_fmt
+        elif record.levelno == 20: # INFO
+            self._fmt = ColorFormatter.info_fmt
+        elif record.levelno == 30: # WARN
+            self._fmt = ColorFormatter.warn_fmt
+        elif record.levelno == 40: # ERROR
+            self._fmt = ColorFormatter.err_fmt
+        # Call the original formatter class to do the grunt work
+        result = logging.Formatter.format(self, record)
+        # Restore the original format configured by the user
+        self._fmt = format_orig
+        return result 
 
 def _setup_logging(env):
     env.logger = logging.getLogger("cloudbiolinux")
@@ -10,10 +43,8 @@ def _setup_logging(env):
 
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    # create formatter
-    formatter = logging.Formatter('%(name)s %(levelname)s: %(message)s')
-    # add formatter to ch
-    ch.setFormatter(formatter)
+    # Use custom formatter
+    ch.setFormatter(ColorFormatter())
     env.logger.addHandler(ch)
 
 def _update_biolinux_log(env, target, flavor):
