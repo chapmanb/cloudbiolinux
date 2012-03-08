764908ee19bbb34bf33213307ef256e1ab6edd35
Luca Pireddu <pireddu@crs4.it>
2012-03-08 15:10:35 +0200

    Completes installation of Seal on Scientific Linux

--- contrib/flavor/seal/sealflavor.py
+++ contrib/flavor/seal/sealflavor.py
@@ -7,6 +7,40 @@ from cloudbio.custom.shared import (_fetch_and_unpack)
 
 import sys
 
+# This flavour installs the Seal toolkit for processing high-throughput
+# sequencing data on Hadoop.
+#   http://biodoop-seal.sf.net/
+#
+# It pulls in quite a few dependencies, including Hadoop itself and
+# Pydoop (http://pydoop.sf.net/).
+#
+# The dependencies it pulls into Cloudbiolinux are structured as follows:
+#
+# contrib/flavor/seal/main.yaml
+#   sealdist
+#   customsealdist
+#
+# config/packages-yum.yaml
+#   sealdist (metapackage)
+# config/custom.yaml
+#   customsealdist (metapackage)
+#     - pydoop
+#     - seal
+#
+# The components of the customsealdist metapackage are installed through
+# the functions in cloudbio/custom/customsealdist.py
+#
+#
+# This flavour has only been installed on Scientific Linux and has not
+# yet been well tested.
+#
+# To try installing it run the following:
+#   cd <your cloudbiolinux directory>
+#   fab -f ./fabfile.py -H root@<your host> -c ./contrib/flavor/seal/fabricrc_sl.txt  install_biolinux:packagelist=contrib/flavor/seal/main.yaml
+#
+# Authors:  Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
+#           Luca Pireddu <luca.pireddu@crs4.it>
+
 class SealFlavor(Flavor):
 	"""A flavour for installing Seal
 	"""
@@ -15,27 +49,7 @@ class SealFlavor(Flavor):
 		self.name = "Seal Flavor"
 
 	def rewrite_config_items(self, name, packages):
-		env.logger.info("SealFlavor#rewrite_config_items:  config item name %s" % name)
 		if name == 'packages':
-			env.logger.info("Hello from SealFlavor.  Package list:\n")
-			for package in packages:
-				env.logger.info("Selected: "+name+" "+package)
-			packages.extend([
-			  "ant",
-			  "ant-junit",
-			  "ant-nodeps",
-			  "java-1.6.0-openjdk",
-			  "protobuf-c",
-			  "protobuf-compiler",
-			  "protobuf-python",
-			  "python",
-			  "python-devel",
-			  "junit4",
-			  "boost-python-devel",
-			  "gcc-c++",
-			  "openssl-devel",
-				])
-
 			if sys.version_info < (2,7):
 				# for versions of Python prior to 2.7 we need to add importlib
 				# and argparse
@@ -43,10 +57,8 @@ class SealFlavor(Flavor):
 					"python-importlib",
 					"python-argparse"
 				])
-			return packages
-		elif name == 'custom':
-			env.logger.debug("rewrite_config_items for custom.  packages is: %s" % str(packages))
-			return packages + [ 'pydoop' ]
+		return packages
+
 
 	def post_install(self):
 		env.logger.info("Starting post-install")
