6d6ad9b14113a73ca48680e43981f92d696b6e9c
Enis Afgan <afgane@gmail.com>
2013-01-07 18:04:54 +1100

    Log which config file is being loaded/used

--- cloudbio/flavor/config.py
+++ cloudbio/flavor/config.py
@@ -1,11 +1,11 @@
-"""Handle alternative configuration file locations for flavor customizations.
+"""
+Handle alternative configuration file locations for flavor customizations.
 """
 import os
 import collections
 
 def _find_fname(env, fname):
     for dirname in [env.get("flavor_dir", None), env.config_dir]:
-        print dirname, fname
         if dirname:
             full_path = os.path.join(dirname, fname)
             if os.path.exists(full_path):
@@ -13,7 +13,8 @@ def _find_fname(env, fname):
     return None
 
 def get_config_file(env, fname):
-    """Retrieve YAML configuration file from the default config directory or flavor directory.
+    """
+    Retrieve YAML configuration file from the default config directory or flavor directory.
 
     This combines all options for getting distribution or flavor specific customizations.
     """
@@ -21,5 +22,5 @@ def get_config_file(env, fname):
     distribution_fname = "{0}-{1}{2}".format(base, env.get("distribution", "notspecified"), ext)
     Config = collections.namedtuple("Config", "base dist")
     out = Config(base=_find_fname(env, fname), dist=_find_fname(env, distribution_fname))
+    env.logger.debug("Using config file {0}".format(out.base))
     return out
-    
