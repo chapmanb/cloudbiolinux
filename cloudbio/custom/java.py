6b86cfda441190d681bc8f497dcbd6e4fcd83c1b
chapmanb <chapmanb@50mail.com>
2013-01-30 16:18:19 -0500

    Update leiningen target to use latest stable release

--- cloudbio/custom/java.py
+++ cloudbio/custom/java.py
@@ -19,13 +19,13 @@ def install_cljr(env):
     run("rm cljr-installer.jar")
 
 @_if_not_installed("lein")
-def install_leinengin(env):
+def install_leiningen(env):
     """Clojure tool for project configuration and automation.
     http://github.com/technomancy/leiningen
     """
     with _make_tmp_dir() as work_dir:
         with cd(work_dir):
-            run("wget --no-check-certificate https://raw.github.com/technomancy/leiningen/preview/bin/lein")
+            run("wget --no-check-certificate https://raw.github.com/technomancy/leiningen/stable/bin/lein")
             run("chmod a+rwx lein")
             env.safe_sudo("mv lein %s" % os.path.join(env.system_install, "bin"))
-            run("lein self-install")
+            run("lein")
