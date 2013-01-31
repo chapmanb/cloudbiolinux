85b3203ef0f44926b00acc7f5d431cfa7a168732
John Chilton <jmchilton@gmail.com>
2013-01-31 09:15:20 -0600

    Deployer: Allow override vm_host (e.g. aws, openstack, etc...) from command-line.

--- cloudbio/deploy/main.py
+++ cloudbio/deploy/main.py
@@ -14,6 +14,7 @@ ARG_PROPERTIES = [
   "runtime_properties",
   "target",
   "flavor",
+  "vm_host",
 ]
 
 
@@ -42,6 +43,7 @@ def parse_args():
     parser.add_argument('--file', dest="files", action="append", default=[], help="file to transfer to new instance")
     parser.add_argument("--target", dest="target", default=DEFAULT_CLOUDBIOLINUX_TARGET)
     parser.add_argument("--flavor", dest="flavor", default=DEFAULT_CLOUDBIOLINUX_FLAVOR)
+    parser.add_argument("--vm_host", dest="vm_host", default=None, help="libcloud driver to use (or vagrant) (e.g. aws, openstack)")
     args = parser.parse_args()
     if len(args.actions) == 0:
         args.actions = ["transfer"]
