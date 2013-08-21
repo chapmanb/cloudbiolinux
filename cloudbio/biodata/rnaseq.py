d8b31b339c4ac58923543793fac56a5b638ef6c6
chapmanb <chapmanb@50mail.com>
2013-08-21 09:44:07 -0400

    Provide versioned downloads for RNA-seq transcript information. Add latest download for GRCh37 from @roryk

--- cloudbio/biodata/rnaseq.py
+++ cloudbio/biodata/rnaseq.py
@@ -7,20 +7,32 @@ http://cufflinks.cbcb.umd.edu/igenomes.html
 """
 import os
 
-from fabric.api import *
-from fabric.contrib.files import *
+from fabric.api import (cd, warn_only)
+
+VERSIONS = {"GRCh37": "-2013-08-21"}
 
 def download_transcripts(genomes, env):
     folder_name = "rnaseq"
-    base_url = "https://s3.amazonaws.com/biodata/annotation/{gid}-rnaseq.tar.xz"
     genome_dir = os.path.join(env.data_files, "genomes")
     for (orgname, gid, manager) in ((o, g, m) for (o, g, m) in genomes
                                     if m.config.get("rnaseq", False)):
+        version = VERSIONS.get(gid, "")
+        base_url = "https://s3.amazonaws.com/biodata/annotation/{gid}-rnaseq{version}.tar.xz"
         org_dir = os.path.join(genome_dir, orgname)
         tx_dir = os.path.join(org_dir, gid, folder_name)
-        if not env.safe_exists(tx_dir):
+        version_dir = tx_dir + version
+        if not env.safe_exists(version_dir):
             with cd(org_dir):
-                _download_annotation_bundle(env, base_url.format(gid=gid), gid)
+                _download_annotation_bundle(env, base_url.format(gid=gid, version=version), gid)
+                if version:
+                    _symlink_version(env, tx_dir, version_dir)
+
+def _symlink_version(env, tx_dir, version_dir):
+    """Symlink the expected base output directory to our current version.
+    """
+    if env.safe_exists(tx_dir):
+        env.safe_run("rm -rf %s" % tx_dir)
+    env.safe_run("ln -s %s %s" % (version_dir, tx_dir))
 
 def _download_annotation_bundle(env, url, gid):
     """Download bundle of RNA-seq data from S3 biodata/annotation
