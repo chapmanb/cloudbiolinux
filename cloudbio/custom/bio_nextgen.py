e5d0e7819c1d819f38428a06483ca6d5960640ae
Brad Chapman <chapmanb@50mail.com>
2013-09-30 19:12:32 -0500

    Bump freebayes to latest version. Resolves issues with NA12878 test calling

--- cloudbio/custom/bio_nextgen.py
+++ cloudbio/custom/bio_nextgen.py
@@ -724,8 +724,8 @@ def install_freebayes(env):
     """Bayesian haplotype-based polymorphism discovery and genotyping.
     https://github.com/ekg/freebayes
     """
-    version = "0.9.9.2-10"
-    revision = "3e07445a9"
+    version = "0.9.9.2-14"
+    revision = "10ac8d449"
     if versioncheck.up_to_date(env, "freebayes", version, stdout_flag="version:"):
         return
     repository = "git clone --recursive https://github.com/ekg/freebayes.git"
