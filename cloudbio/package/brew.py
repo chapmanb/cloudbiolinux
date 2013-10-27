19ed58786f9fbb6490dd2eccb49f4957020f9b84
chapmanb <chapmanb@50mail.com>
2013-10-27 11:16:47 -0400

    Enables pass through of current on PATH gcc and g++ compilers to brew. Requires Homebrew/linuxbrew#29

--- cloudbio/package/brew.py
+++ cloudbio/package/brew.py
@@ -114,7 +114,8 @@ def _install_pkg_latest(env, pkg, brew_cmd, ipkgs):
         brew_subcmd = "install"
     if brew_subcmd:
         perl_setup = "export PERL5LIB=%s/lib/perl5:${PERL5LIB}" % env.system_install
-        env.safe_run("%s && %s %s %s" % (perl_setup, brew_cmd, brew_subcmd, pkg))
+        compiler_setup = "export CC=${CC:-`which gcc`} && export CXX=${CXX:-`which g++`}"
+        env.safe_run("%s && %s && %s %s --env=std %s" % (compiler_setup, perl_setup, brew_cmd, brew_subcmd, pkg))
         env.safe_run("%s link --overwrite %s" % (brew_cmd, pkg))
 
 def _get_pkg_and_version(pkg_str):
