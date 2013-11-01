b34d25f69a7b88e56c908ed036da2bcafad53263
chapmanb <chapmanb@50mail.com>
2013-11-01 05:42:20 -0400

    Remove custom GNU parallel custom target, replacing with brew GNU parallel build. Fixes chapmanb/bcbio-nextgen#160

--- cloudbio/custom/distributed.py
+++ cloudbio/custom/distributed.py
@@ -8,15 +8,6 @@ from fabric.contrib.files import *
 from shared import (_if_not_installed, _if_not_python_lib,
                     _pip_cmd, _get_install, _configure_make, _is_anaconda)
 
-@_if_not_installed("parallel -h")
-def install_gnu_parallel(env):
-    """GNU parallel: build and execute command lines from standard input in parallel.
-    https://savannah.gnu.org/projects/parallel/
-    """
-    version = "20130122"
-    url = "ftp://ftp.gnu.org/gnu/parallel/parallel-%s.tar.bz2" % version
-    _get_install(url, env, _configure_make)
-
 @_if_not_python_lib("pydoop")
 def install_pydoop(env):
     """pydoop; provides Hadoop access for Python.
