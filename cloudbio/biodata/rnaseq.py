8ac0222b61fdb65df05d31d4504d68af05f34b1d
chapmanb <chapmanb@50mail.com>
2014-02-24 12:53:36 -0500

    Update to latest releases of RNA-seq data with Ensembl 74 annotations

--- cloudbio/biodata/rnaseq.py
+++ cloudbio/biodata/rnaseq.py
@@ -13,8 +13,8 @@ from cloudbio.custom import shared
 from cloudbio.fabutils import warn_only
 
 VERSIONS = {"rn5": "2014-02-13",
-            "GRCh37": "2014-02-13",
-            "hg19": "2014-02-13",
+            "GRCh37": "2014-02-12",
+            "hg19": "2014-02-14",
             "mm10": "2014-02-13"}
 
 def download_transcripts(genomes, env):
