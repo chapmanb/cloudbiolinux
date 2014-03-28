8a0017ff2b355226502b979c2daa02ced25a821b
chapmanb <chapmanb@50mail.com>
2014-03-28 15:25:06 -0400

    Avoid standard seal/pydoop installation. Requires new dependencies and best suited for a custom flavor devoted to Cloud/Hadoop

--- cloudbio/custom/distributed.py
+++ cloudbio/custom/distributed.py
@@ -5,15 +5,14 @@ import os
 from fabric.api import *
 from fabric.contrib.files import *
 
-from shared import (_if_not_installed, _if_not_python_lib,
-                    _pip_cmd, _get_install, _configure_make, _is_anaconda)
+from shared import (_if_not_python_lib, _pip_cmd, _is_anaconda)
 
 @_if_not_python_lib("pydoop")
 def install_pydoop(env):
     """pydoop; provides Hadoop access for Python.
     http://pydoop.sourceforge.net/docs/
     """
-    java_home = env.java_home if env.has_key("java_home") else os.environ["JAVA_HOME"]
+    java_home = env.java_home if "java_home" in env else os.environ["JAVA_HOME"]
     export_str = "export JAVA_HOME=%s" % (java_home)
     cmd = env.safe_run if _is_anaconda(env) else env.safe_sudo
     cmd("%s && %s install pydoop" % (export_str, _pip_cmd(env)))
@@ -26,7 +25,7 @@ def install_seal(env):
     """
     install_pydoop(env)
 
-    java_home = env.java_home if env.has_key("java_home") else os.environ["JAVA_HOME"]
+    java_home = env.java_home if "java_home" in env else os.environ["JAVA_HOME"]
     export_str = "export JAVA_HOME=%s" % (java_home)
     cmd = env.safe_run if _is_anaconda(env) else env.safe_sudo
     cmd("%s && %s install --pre seal" % (export_str, _pip_cmd(env)))
