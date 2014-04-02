52f73a538b36f3de97be5bc17cd351b6ddacbee3
Roman Valls Guimera <roman@incf.org>
2014-04-02 12:58:00 +0200

    Flavor for neurodebian, a debian neuroscience package repository

---
+++ contrib/flavor/neuro/neuro.py
@@ -0,0 +1,33 @@
+from fabric.api import *
+from fabric.contrib.files import *
+
+from cloudbio.flavor import Flavor
+from cloudbio.edition import Edition
+
+# This flavour installs several neuroinformatics software from NeuroDebian
+#
+#   http://neuro.debian.net/
+#
+# Author:  Roman Valls Guimera <roman@incf.org>
+
+class NeuroFlavor(Flavor):
+    """ A flavour for installing NeuroDebian. A debian neuroinformatics repository
+	    of software and datasets.
+	"""
+
+    def __init__(self, env):
+        Flavor.__init__(self, env)
+        self.name = "Neuroinformatics Flavor"
+
+    def rewrite_apt_sources_list(self, name, sources):
+        sources = [
+            'deb http://neurodeb.pirsquared.org data main contrib non-free',
+            '#deb-src http://neurodeb.pirsquared.org data main contrib non-free',
+            'deb http://neurodeb.pirsquared.org saucy main contrib non-free',
+            '#deb-src http://neurodeb.pirsquared.org saucy main contrib non-free'
+        ]
+
+#wget -O- http://neuro.debian.net/lists/saucy.us-ca.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+#sudo apt-key adv --recv-keys --keyserver pgp.mit.edu 2649A5A9
+
+env.flavor = NeuroFlavor(env)
