4b2421a4f05a8e9b5f9712e1ba670a3bf99d4a90
glebk <gleb.kuznetsov@gmail.com>
2014-04-17 17:51:37 -0400

    Correct flag value to install_brew() automated=True.
    That's what I REALLY meant in 6375737108abc4de5a529685930f4de7c5a131bf.

--- fabfile.py
+++ fabfile.py
@@ -110,7 +110,7 @@ def _perform_install(target=None, flavor=None, more_custom_add=None):
     if target is None or target == "puppet_classes":
         _provision_puppet_classes(pkg_install, custom_ignore)
     if target is None or target == "brew":
-        install_brew(flavor=flavor, automated=False)
+        install_brew(flavor=flavor, automated=True)
     if target is None or target == "libraries":
         _do_library_installs(lib_install)
     if target is None or target == "post_install":
