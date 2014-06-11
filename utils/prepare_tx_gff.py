2c1fce3982a083c88787d85bffd6e3af9f26a848
roryk <rory.kirchner@gmail.com>
2014-06-11 17:01:49 -0400

    Remove automatic uploading to S3.

--- utils/prepare_tx_gff.py
+++ utils/prepare_tx_gff.py
@@ -198,7 +198,6 @@ def main(org_build, gtf_file=None):
         prepare_tophat_index(gtf_file, org_build)
         cleanup(work_dir, out_dir, org_build)
     tar_dirs = [out_dir]
-    upload_to_s3(tar_dirs, org_build)
 
 
 def cleanup(work_dir, out_dir, org_build):
