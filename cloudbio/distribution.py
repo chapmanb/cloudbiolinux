e4dc798087072a7909bf89613a0fcad7b0fbc632
chapmanb <chapmanb@50mail.com>
2014-12-12 07:37:36 -0500

    More flexible detection of Scientific Linux for SL systems. Thanks to Mick Watson

--- cloudbio/distribution.py
+++ cloudbio/distribution.py
@@ -284,7 +284,7 @@ def _determine_distribution(env):
         return "centos"
     elif output.find("amzn") >= 0:  # Amazon AMIs are Red-Hat based
         return "centos"
-    elif output.find("scientific linux release") >= 0:
+    elif output.find("scientific linux") >= 0:
         return "scientificlinux"
     elif env.safe_exists("/etc/debian_version"):
         return "debian"
