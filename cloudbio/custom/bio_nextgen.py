98bc0370444388a3be551c7a043cb31b93814fd7
chapmanb <chapmanb@50mail.com>
2014-12-17 21:20:31 -0500

    bcbio.variation: v0.2.0

--- cloudbio/custom/bio_nextgen.py
+++ cloudbio/custom/bio_nextgen.py
@@ -678,7 +678,7 @@ def install_bcbio_variation(env):
     """Toolkit to analyze genomic variation data with comparison and ensemble approaches.
     https://github.com/chapmanb/bcbio.variation
     """
-    version = "0.1.9"
+    version = "0.2.0"
     url = "https://github.com/chapmanb/bcbio.variation/releases/download/" \
           "v%s/bcbio.variation-%s-standalone.jar" % (version, version)
     install_dir = _symlinked_java_version_dir("bcbio_variation", version, env)
