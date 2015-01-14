169ebc870ee239b409bc2e303fd526340b284313
chapmanb <chapmanb@50mail.com>
2015-01-14 06:44:00 -0500

    Additional perl libraries for VEP, thanks to @dwaggott. Fixes chapmanb/homebrew-cbl#17

--- cloudbio/package/brew.py
+++ cloudbio/package/brew.py
@@ -277,7 +277,8 @@ def _install_brew_baseline(env, brew_cmd, ipkgs, packages):
         _install_pkg_latest(env, "cpanminus", [], brew_cmd, ipkgs)
         cpanm_cmd = os.path.join(os.path.dirname(brew_cmd), "cpanm")
         for perl_lib in ["Statistics::Descriptive", "Archive::Extract", "Archive::Zip", "Archive::Tar", "DBI",
-                         "LWP::Simple", "LWP::Protocol::https", "Time::HiRes", "IPC::Cmd"]:
+                         "LWP::Simple", "LWP::Protocol::https", "Time::HiRes", "IPC::Cmd",
+                         "Params::Check", "Module::Load::Conditional"]:
             env.safe_run("%s -i --notest --local-lib=%s '%s'" % (cpanm_cmd, env.system_install, perl_lib))
     # Ensure paths we may have missed on install are accessible to regular user
     if env.use_sudo:
