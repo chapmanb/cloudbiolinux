a60dbba8a04e55d0362e2e941e448311024e9154
chapmanb <chapmanb@50mail.com>
2015-06-19 11:35:58 -0400

    Correctly skip conda package installations if configuration file not present. Fixes #199

--- cloudbio/package/conda.py
+++ cloudbio/package/conda.py
@@ -10,10 +10,13 @@ def install_packages(env, to_install=None, packages=None):
     if shared._is_anaconda(env):
         conda_bin = shared._conda_cmd(env)
         config_file = get_config_file(env, "packages-conda.yaml")
-        if to_install:
-            (packages, _) = _yaml_to_packages(config_file.base, to_install, config_file.dist)
-        with open(config_file.base) as in_handle:
-            channels = " ".join(["-c %s" % x for x in yaml.safe_load(in_handle).get("channels", [])])
+        if config_file.base is None and packages is None:
+            packages = []
+        else:
+            if to_install:
+                (packages, _) = _yaml_to_packages(config_file.base, to_install, config_file.dist)
+            with open(config_file.base) as in_handle:
+                channels = " ".join(["-c %s" % x for x in yaml.safe_load(in_handle).get("channels", [])])
         if len(packages) > 0:
             pkgs_str = " ".join(packages)
             env.safe_run("{conda_bin} install -y {channels} {pkgs_str}".format(**locals()))
