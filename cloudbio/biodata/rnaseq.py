521c0cc7564d3aa64f0c71e74db7fd33a453f8b4
Lorena <lorena.pantano@gmail.com>
2015-07-31 12:16:49 -0400

    add smallRNA-seq data

--- cloudbio/biodata/rnaseq.py
+++ cloudbio/biodata/rnaseq.py
@@ -17,6 +17,8 @@ VERSIONS = {"rn5": "2014-07-20",
             "hg19": "2014-07-17",
             "mm10": "2014-07-14",
             "canFam3": "2014-07-20"}
+VERSIONS_SMALLRNA = {"hg19": "2015-08-01",
+                     "mm10": "2015-08-01"}
 
 def download_transcripts(genomes, env):
     folder_name = "rnaseq"
@@ -35,6 +37,18 @@ def download_transcripts(genomes, env):
                     _symlink_version(env, tx_dir, version_dir)
         if version:
             _symlink_refgenome(env, gid, org_dir)
+
+        version_srnaseq = VERSIONS_SMALLRNA.get(gid, "")
+        if version_srnaseq:
+            tx_dir = os.path.join(org_dir, gid, "srnaseq")
+            version_dir = "%s-%s" % (tx_dir, version_srnaseq)
+            gtf_file = os.path.join(version_dir, "srna-transcripts.gtf")
+            if not env.safe_exists(gtf_file):
+                env.safe_sudo("mkdir -p %s" % version_dir, user=env.user)
+                with cd(version_dir):
+                    has_srnaseq = _download_srnaseq(env, version_dir, gid)
+                    if has_srnaseq:
+                        _symlink_version(env, tx_dir, version_dir)
 
 def _symlink_refgenome(env, gid, org_dir):
     """Provide symlinks back to reference genomes so tophat avoids generating FASTA genomes.
@@ -68,3 +82,19 @@ def _download_annotation_bundle(env, url, gid):
     else:
         env.logger.warn("RNA-seq transcripts not available for %s" % gid)
         return False
+
+def _download_srnaseq(env, version_dir, gid):
+    """Download bundle of small RNA-seq data from seqcluster installation
+    """
+    url = "http://raw.githubusercontent.com/lpantano/seqcluster/master/scripts/%s.sh" % gid
+    env.logger.warn(url)
+    bash_script = shared._remote_fetch(env, url, allow_fail=True)
+    if bash_script and env.safe_exists(bash_script):
+        env.logger.info("Extracting small RNA-seq references: %s" % gid)
+        env.safe_run("bash %s" % bash_script)
+        env.safe_run("rm -f %s" % bash_script)
+        return True
+    else:
+        env.logger.warn("small RNA-seq transcripts not available for %s" % gid)
+        return False
+
