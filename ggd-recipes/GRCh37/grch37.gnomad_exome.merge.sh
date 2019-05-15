9a71549a80062b13d5212d97d3ab803d11f4e469
Matthias De Smet <matthias.desmet@ugent.be>
2019-05-15 11:44:03 +0200

    Revert "cleanup debug scripts"
    This reverts commit d3acec7d209657d6bf706e81d08a607220f8a80e.

---
+++ ggd-recipes/GRCh37/grch37.gnomad_exome.merge.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PBS -l walltime=23:00:00,nodes=1:ppn=1
+#PBS -joe .
+#PBS -d .
+#PBS -l vmem=10g,mem=10g
+
+date
+
+export PATH=/hpf/largeprojects/ccmbio/naumenko/tools/bcbio_1.1.5/bin:$PATH
+export TMPDIR=`pwd`
+cd variation
+bcftools concat gnomad_exome.chr1.vcf.gz gnomad_exome.chr2.vcf.gz gnomad_exome.chr3.vcf.gz gnomad_exome.chr4.vcf.gz gnomad_exome.chr5.vcf.gz gnomad_exome.chr6.vcf.gz \
+gnomad_exome.chr7.vcf.gz gnomad_exome.chr8.vcf.gz gnomad_exome.chr9.vcf.gz gnomad_exome.chr10.vcf.gz gnomad_exome.chr11.vcf.gz gnomad_exome.chr12.vcf.gz \
+gnomad_exome.chr13.vcf.gz gnomad_exome.chr14.vcf.gz gnomad_exome.chr15.vcf.gz gnomad_exome.chr16.vcf.gz gnomad_exome.chr17.vcf.gz gnomad_exome.chr18.vcf.gz \
+gnomad_exome.chr19.vcf.gz gnomad_exome.chr20.vcf.gz gnomad_exome.chr21.vcf.gz gnomad_exome.chr22.vcf.gz gnomad_exome.chrX.vcf.gz gnomad_exome.chrY.vcf.gz -Ov | bgzip -c > gnomad_exome.vcf.gz 
+
+tabix -f -p vcf gnomad_exome.vcf.gz
+tabix -f -p vcf --csi gnomad_exome.vcf.gz
+
+cd ..
+date
